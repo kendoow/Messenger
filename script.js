68ece8762a130edef5825dbfa32f4c0fc55a128e
kendo <kendo_ow@mail.ru>
2021-01-22 23:08:23 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var textarea = document.querySelector('textarea');
+const textarea = document.querySelector('textarea');
 
 textarea.addEventListener('keyup', function(){
   if(this.scrollTop > 0){
     this.style.height = this.scrollHeight + "px";
   }
 });
+
+const menuBtn = document.querySelector(".m-menu");
+const menu = document.querySelector(".main__menu");
+
+
+menuBtn.addEventListener('click', function(){
+  menu.classList.toggle('active');
+});
+
